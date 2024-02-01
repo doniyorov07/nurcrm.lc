cb2902cdc90d7f5e9645b47c40895b736f4d3c0c
doniyorovuz <doniyorovrozimurod1@gmail.com>
2024-02-01 22:51:58 +0500

    .

--- common/models/forms/TeacherGroupForm.php
+++ common/models/forms/TeacherGroupForm.php
@@ -2,7 +2,6 @@
 
 namespace common\models\forms;
 
-
 use common\models\Group;
 use common\models\ModelToData;
 use common\models\Teacher;
@@ -12,16 +11,11 @@ use yii\base\Model;
 
 class TeacherGroupForm extends Model
 {
-
-    public  TeacherGroup $model;
-
+    public TeacherGroup $model;
     public ?int $teacher_id;
     public ?int $group_id;
     public ?string $created_teacher_at;
     public ?string $updated_teacher_at;
-
-
-
 
     public function __construct(TeacherGroup $model, $config = [])
     {
@@ -40,25 +34,12 @@ class TeacherGroupForm extends Model
         $model->teacher_id = $this->teacher_id;
         $model->group_id = $this->group_id;
 
-        $TeacherGroup = TeacherGroup::findOne(['group_id' => $this->group_id]);
-
-        if ($TeacherGroup !== null) {
-            if ($TeacherGroup->teacher_id !== $this->teacher_id) {
-                return false;
-            }
-        }
-
         if ($model->isNewRecord) {
             $model->created_teacher_at = date('Y-m-d H:i:s');
         }
-
         $model->updated_teacher_at = date('Y-m-d H:i:s');
-        $model->init();
-
         return $model->save(false);
     }
-
-
 
 
 
@@ -71,5 +52,4 @@ class TeacherGroupForm extends Model
             [['teacher_id'], 'exist', 'skipOnError' => true, 'targetClass' => Teacher::class, 'targetAttribute' => ['teacher_id' => 'id']],
         ];
     }
-
 }
